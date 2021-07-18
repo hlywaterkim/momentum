639d7820a8d3a045491834052e5356be963f7a1e
Sungsu Kim <kim.sungsu6732@gmail.com>
2021-07-19 08:34:46 +0900

    fix error on .html, .js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  const username = loginInput.nodeValue;
+  const username = loginInput.value;
   console.log(username);
 }
 
