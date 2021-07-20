9e13b804c110e15f2f849e7f5c409b4333264642
Sungsu Kim <kim.sungsu6732@gmail.com>
2021-07-21 08:20:50 +0900

    #4.3 Events part.2

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   console.log(username);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
